07e88d287ed6562d696549dcd3e014dec4195a64
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-04-11 16:21:24 +0530

    Update fetchdata.php

--- fetchdata.php
+++ fetchdata.php
@@ -2,19 +2,19 @@
     function filldiv(){
         $loopResult = "";
         $conn = new mysqli("localhost", "root", "", "newnotes");
-        $sql = "SELECT * FROM newnotesdetails";
+        $sql = "SELECT * FROM newnotesdetails ORDER BY ind DESC";
         $result = $conn->query($sql);
         while($row = $result->fetch_assoc()){
             $loopResult .= '
                 <div class="col-12 col-md-4 my-2">
                     <div class="card shadow h-100">
-                        <img src="'.$row['src'].'" alt="'.$row['alt'].'" class="card-img-top">
+                        <img src="'.stripslashes($row['src']).'" alt="'.stripslashes($row['alt']).'" class="card-img-top">
                         <div class="card-body">
                             <h4 class="card-title">
-                                '.$row['title'].'
+                                '.stripslashes($row['title']).'
                             </h4>
-                            <p class="card-text">'.$row['text'].'</p>
-                            <a href="'.$row['href'].'" class="stretched-link"></a>
+                            <p class="card-text">'.stripslashes($row['text']).'</p>
+                            <a href="'.stripslashes($row['href']).'" class="stretched-link"></a>
                         </div>
                     </div>
                 </div> 
